203e7da839e6fc4f0320af4735db6cff8a9adba3
Danh <thanhdanh010499@gmail.com>
2023-07-25 14:19:35 +0700

    fix syntax code

--- app_joke/src/component/footer.compoent.tsx
+++ app_joke/src/component/footer.compoent.tsx
@@ -1,21 +1,19 @@
-import React from 'react'
-
 export default function Footer() {
   return (
-    <footer className=" text-center ">
-    <div className="p-6 text-neutral-800">
-      <div className="">
-        <div className="mb-6 md:mb-0 ">
-          <p className="mb-4 text-center pl-[22%] w-[80%]">
-          This website is created as past of Hlsolutions program. The materials contained on this website are provided for general infomation only and do not constitue any form of advice. HLS assumes no responsibilly for the accuracy of any particular statement and accepts no liability for any loss or damage which may arise from reliance on the information contained on this site
-          </p>
+    <footer className=' text-center '>
+      <div className='p-6 text-neutral-800'>
+        <div className=''>
+          <div className='mb-6 md:mb-0 '>
+            <p className='mb-4 text-center pl-[22%] w-[80%]'>
+              This website is created as past of Hlsolutions program. The materials contained on this website are
+              provided for general infomation only and do not constitue any form of advice. HLS assumes no responsibilly
+              for the accuracy of any particular statement and accepts no liability for any loss or damage which may
+              arise from reliance on the information contained on this site
+            </p>
+          </div>
         </div>
-        </div>
-        </div>
-    <div
-      className=" text-center text-neutral-700">
-      © 2023 Copyright: HLS
-    </div>
-  </footer>
+      </div>
+      <div className=' text-center text-neutral-700'>© 2023 Copyright: HLS</div>
+    </footer>
   )
 }
--- app_joke/src/component/joke.component.tsx
+++ app_joke/src/component/joke.component.tsx
@@ -1,25 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Header from './header.component'
 import Footer from './footer.compoent'
 
+const contents = [
+  "A child asked his father, 'How were people born?' So his father said, 'Adam and Eve made babies, then their babies became adults and made babies, and so on'.The child then went to his mother, asked her the same question and she told him, 'We were monkeys then we evolved to become like we are now'.The child ran back to his father and said, 'You lied to me! His father replied, No, your mom was talking about her side of the family.'",
+  "Teacher: 'Kids,what does the chicken give you?' Student: 'Meat'! Teacher: 'Very good! Now what does the pig give you?' Student: 'Bacon!' Teacher: 'Great! And what does the fat cow give you? Student: Homework!'",
+  "The teacher asked Jimmy, 'Why is your cat at school today Jimmy?' Jimmy replied crying, 'Because I heard my daddy tell my mommy', 'I am going to eat that pussy once Jimmy leaves for school today!'",
+  "A housewife, an accountant and a lawyer were asked 'How much is 2+2?' The housewife replies: 'Four!'. The accountant says: 'I think it's either 3 or 4. Let me run those figures through my spreadsheet one more time.' The lawyer pulls the drapes, dims the lights and asks in a hushed voice, 'How much do you want it to be?'"
+]
+
 export default function JokeApp() {
-  const contents = [
-    "A child asked his father, 'How were people born?' So his father said, 'Adam and Eve made babies, then their babies became adults and made babies, and so on'.The child then went to his mother, asked her the same question and she told him, 'We were monkeys then we evolved to become like we are now'.The child ran back to his father and said, 'You lied to me! His father replied, No, your mom was talking about her side of the family.'",
-    "Teacher: 'Kids,what does the chicken give you?' Student: 'Meat'! Teacher: 'Very good! Now what does the pig give you?' Student: 'Bacon!' Teacher: 'Great! And what does the fat cow give you? Student: Homework!'",
-    "The teacher asked Jimmy, 'Why is your cat at school today Jimmy?' Jimmy replied crying, 'Because I heard my daddy tell my mommy', 'I am going to eat that pussy once Jimmy leaves for school today!'",
-    "A housewife, an accountant and a lawyer were asked 'How much is 2+2?' The housewife replies: 'Four!'. The accountant says: 'I think it's either 3 or 4. Let me run those figures through my spreadsheet one more time.' The lawyer pulls the drapes, dims the lights and asks in a hushed voice, 'How much do you want it to be?'"
-  ]
   const [contentIndex, setContexIndex] = useState(0)
   const [funnyCount, setFunnyCount] = useState(0)
   const [notFunnyCount, setNotFunnyCount] = useState(0)
-  
-  
-  useEffect(() =>{
-    setCookie('funnyCount', funnyCount, 30);
-    setCookie('notFunnyCount', notFunnyCount, 30);
-  },[funnyCount,notFunnyCount])
 
-  
+  useEffect(() => {
+    setCookie('funnyCount', funnyCount, 30)
+    setCookie('notFunnyCount', notFunnyCount, 30)
+  }, [funnyCount, notFunnyCount])
+
   const handleThisFunny: any = () => {
     setContexIndex(contentIndex + 1)
     setFunnyCount(funnyCount + 1)
@@ -30,12 +29,12 @@ export default function JokeApp() {
     setNotFunnyCount(notFunnyCount + 1)
   }
 
-  const setCookie: any = (name: any, value : any, days: any) => {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    const expires = "expires=" + date.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
-  };
+  const setCookie: any = (name: any, value: any, days: any) => {
+    const date = new Date()
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
+    const expires = 'expires=' + date.toUTCString()
+    document.cookie = name + '=' + value + ';' + expires + ';path=/'
+  }
 
   return (
     <div className=''>
